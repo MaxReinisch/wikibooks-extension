8cbc388a4b9d0ff6aaf3e333aab221938d44ffe0
MaxReinisch <mreinisc@oberlin.edu>
2018-09-10 15:09:34 -0700

    added stylesheet and handles books not on the archive

--- scripts/background.js
+++ scripts/background.js
@@ -1,63 +1,3 @@
-// function findISBNs(body){ // requires further testing
-//   var search_result = body.match(/isbn\D{0,3}[-\s\d]{9,16}\d/ig);
-//   return search_result;
-// }
-//
-// function isValidISBN(str) {
-//   // https://stackoverflow.com/questions/11104439/how-do-i-check-if-an-input-contains-an-isbn-using-javascript
-//   var sum,
-//   weight,
-//   digit,
-//   check,
-//   i;
-//
-//   str = str.replace(/[^0-9X]/gi, '');
-//
-//   if (str.length != 10 && str.length != 13) {
-//     return false;
-//   }
-//
-//   if (str.length == 13) {
-//     sum = 0;
-//     for (i = 0; i < 12; i++) {
-//       digit = parseInt(str[i]);
-//       if (i % 2 == 1) {
-//         sum += 3*digit;
-//       } else {
-//         sum += digit;
-//       }
-//     }
-//     check = (10 - (sum % 10)) % 10;
-//     return (check == str[str.length-1]);
-//   }
-//
-//   if (str.length == 10) {
-//     weight = 10;
-//     sum = 0;
-//     for (i = 0; i < 9; i++) {
-//       digit = parseInt(str[i]);
-//       sum += weight*digit;
-//       weight--;
-//     }
-//     check = 11 - (sum % 11);
-//     if (check == 10) {
-//       check = 'X';
-//     }
-//     return (check == str[str.length-1].toUpperCase());
-//   }
-// } // Replace this.  Failed a valid ISBN: 0-7141-2504-0
-//
-// function validationCheck(isbns){
-//   console.log("found " + isbns.length + " ISBNs.");
-//   validList = isbns.map(isValidISBN);
-//   console.log(validList.reduce((x, y) => x+y) + " are valid.");
-//   console.log("Printing invalid ISBN(s):");
-//   for(let i =0; i < isbns.length; i++){
-//     if(!validList[i]){
-//       console.log(isbns[i]);
-//     }
-//   }
-// }
 
 // Extension only active on wikipedia pages
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
@@ -70,40 +10,33 @@ chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 }]);
 });
 
-// parse JSON response for IDs
-function getIDsFromResponse(data){
-  if(data instanceof Array){
-    for(let book of data){
-      let id = Object.values(book.responses)[0].identifier;
-      ids.push(id);
-    }
-  }else{
-    // console.log(data.message);
-  }
-  return ids;
-}
+// // parse JSON response for IDs
+// function getIDsFromResponse(data){
+//   if(data instanceof Array){
+//     for(let book of data){
+//       let id = Object.values(book.responses)[0].identifier;
+//       ids.push(id);
+//     }
+//   }else{
+//     // console.log(data.message);
+//   }
+//   return ids;
+// }
 
-// requests "/context/books/" endpoint
-function getBooked(url){
-  var xhr=new XMLHttpRequest();
-  var new_url="https://archive.org/services/context/books?url=" + url;
-  xhr.open("GET",new_url,true);
-  xhr.onload=function(){
-    let data=JSON.parse(xhr.response);
-    ids = getIDsFromResponse(data);
-
-    let windowOptions = {
-      "url": "booklist.html",
-      "left":100,
-      "top": 100,
-      "width": 300,
-      "height":1000
-    }
-    chrome.windows.create(windowOptions);
-
-  }
-  xhr.send();
-}
+// // requests "/context/books/" endpoint
+// function getBooked(url){
+//   var xhr=new XMLHttpRequest();
+//   var new_url="https://archive.org/services/context/books?url=" + url;
+//   xhr.open("GET",new_url,true);
+//   xhr.onload=function(){
+//     let data=JSON.parse(xhr.response);
+//     ids = getIDsFromResponse(data);
+//
+//
+//
+//   }
+//   xhr.send();
+// }
 
 
 let ids = []
@@ -113,14 +46,21 @@ chrome.runtime.onMessage.addListener(
     // sent from popup requesting that background.js get
     // book identifiers (IDs), and open booklist.html window
     if(request.action === "getBooked"){
-      getBooked(request.url);
+      let windowOptions = {
+        "url": chrome.runtime.getURL("booklist.html") + "?url="+request.url,
+        "left":100,
+        "top": 100,
+        "width": 500,
+        "height":1000
+      }
+      chrome.windows.create(windowOptions);
 
     }
     // sent from booklist.js requesting that backgrond.js get
     // send book identifiers
-    if(request.action === "populateBooklist"){
-      sendResponse({"IDs": ids})
-      ids = [];
-    }
+    // if(request.action === "populateBooklist"){
+    //   sendResponse({"IDs": ids})
+    //   ids = [];
+    // }
   }
 );
--- scripts/booklist.js
+++ scripts/booklist.js
@@ -1,49 +1,151 @@
 
 let resultsTray = document.getElementById("resultsTray");
-chrome.runtime.sendMessage({"action" : "populateBooklist"}, function(response){
 
-  // For each id, get meta data and append book to list
-  populateList(response);
+//Used to extact the current URL
+function getUrlByParameter(name){
+    var url=window.location.href;
+    var indexOfEnd=url.length;
+    var index=url.indexOf(name);
+    var length=name.length;
+    return url.slice(index+length+1,indexOfEnd);
+}
 
-});
-function getMetadata(id){
+
+function getBooked(url){
+  var xhr=new XMLHttpRequest();
+  var new_url="https://archive.org/services/context/books?url=" + url;
+  xhr.open("GET",new_url,true);
+  xhr.onload=function(){
+    let data=JSON.parse(xhr.response);
+    if(data instanceof Array){ //checks if any ISBNs found
+      for(let book of data){  // Iterate over each book to get data
+        let OLID = Object.keys(book.responses)[0];
+        let archiveIdentifier = book.responses[OLID]['identifier'];
+        if(archiveIdentifier){
+          getMetadataFromArchive(archiveIdentifier);
+        }else{
+          getMetadataFromOpenLibrary(OLID);
+        }
+      }
+    }else{
+      resultsTray.appendChild(document.createTextNode(data.message));
+    }
+  }
+  xhr.send();
+}
+
+// // parse JSON response for IDs
+// function getIDsFromResponse(data){
+//   let OLIDs = [];
+//   if(data instanceof Array){
+//     for(let book of data){
+//       let id = Object.keys(book.responses)[0];
+//       OLIDs.push(id);
+//     }
+//   }else{
+//     resultsTray.appendChild(document.createTextNode(data.message));
+//   }
+//   return OLIDs;
+// }
+//
+
+
+
+
+
+window.onload = function(){
+
+  var url = getUrlByParameter('url');
+  getBooked(url);
+  // populateList(response);
+
+};
+
+function getMetadataFromArchive(id){
 
   var xhr=new XMLHttpRequest();
   xhr.responseType = "json";
   var qurl="https://archive.org/metadata/" + id;
   xhr.open("GET",qurl,true);
   xhr.onload=function(){
-    addBook(xhr.response.metadata);
+    addBookFromArchive(xhr.response.metadata);
 
   }
   xhr.send();
 
 }
 
+function getMetadataFromOpenLibrary(olid){
+  var xhr=new XMLHttpRequest();
+  xhr.responseType = "json";
+  var qurl= "http://openlibrary.org/works/"+olid+".json";
+  console.log(qurl);
+  xhr.open("GET",qurl,true);
+  xhr.onload=function(){
+    addBookFromOpenLibrary(xhr.response);
 
-function populateList(response){
-
-  for (let id of response.IDs){
-    if(id){
-          getMetadata(id);
-    }
   }
+  xhr.send();
 }
 
-function addBook(metadata){
-  let li = document.createElement('li');
-  let book = document.createElement('div');
+function addBookFromArchive(metadata){
+  let div = document.createElement('div');
+  let book = document.createElement('p');
+  let strong = document.createElement('strong');
+  let author = document.createElement('p');
   let details = document.createElement('a');
   let img = document.createElement("img");
+  let button = document.createElement("a");
 
+  button.setAttribute("class", "btn btn-success resize_fit_center");
+  button.setAttribute("href", "https://archive.org/stream/" + metadata.identifier);
   details.setAttribute("href", "https://archive.org/details/" + metadata.identifier);
   img.setAttribute("src", "https://archive.org/services/img/" + metadata.identifier);
-  book.setAttribute("class", "book_container");
+  // book.setAttribute("class", "book_container");
 
   details.appendChild(img);
-  book.appendChild(document.createTextNode(metadata.title));
+  strong.appendChild(document.createTextNode(metadata.title));
+  author.appendChild(document.createTextNode(metadata.creator));
+  button.appendChild(document.createTextNode("Read Now"));
+  book.appendChild(strong);
+  book.appendChild(author)
   book.appendChild(details);
-  li.appendChild(book);
+  book.appendChild(button);
+  div.appendChild(book);
 
-  resultsTray.appendChild(li);
+  resultsTray.appendChild(div);
+}
+
+function addBookFromOpenLibrary(metadata){
+  let div = document.createElement('div');
+  let book = document.createElement('p');
+  let strong = document.createElement('strong');
+  // let author = document.createElement('p');
+  let details = document.createElement('a');
+  let img = document.createElement("img");
+  let button = document.createElement("a");
+
+  button.setAttribute("class", "btn btn-warning resize_fit_center");
+  button.setAttribute("href", "#");
+  details.setAttribute("href", "http://openlibrary.org" + metadata.key);
+  if(metadata.covers){
+    img.setAttribute("src", "http://covers.openlibrary.org/w/id/"+metadata.covers[0]+"-M.jpg");
+  }else{
+    img = document.createElement("p");
+    img.appendChild(document.createTextNode("Book details"))
+  }
+
+  // book.setAttribute("class", "book_container");
+
+  details.appendChild(img);
+  strong.appendChild(document.createTextNode(metadata.title));
+  // author.appendChild(document.createTextNode(metadata.creator));
+  button.appendChild(document.createTextNode("Donate"));
+  book.appendChild(strong);
+  // book.appendChild(author)
+  book.appendChild(details);
+  book.appendChild(button);
+  div.appendChild(book);
+
+  resultsTray.appendChild(div);
 }
